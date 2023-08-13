64f1c79b4c0cd907f7b662aee54b5f977f0b2298
hatuiabhishek <hatuiabhishek@gmail.com>
2023-08-13 12:43:53 +0530

    adding cart button

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,20 @@
+import classses from './Header.module.css';
+import { Fragment } from 'react';
+import mealImg from '../../assests/meals.jpg';
+import HeaderCartButton from './HeaderCartButton';
+
+const Header = () => {
+    return(
+        <Fragment>
+            <header className={classses.header}>
+                <h1>Foodu</h1>
+                <HeaderCartButton />
+            </header>
+            <div className={classses['main-image']}>
+                <img src={mealImg} alt='tasty food'/>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
